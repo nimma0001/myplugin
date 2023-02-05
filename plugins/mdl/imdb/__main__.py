ac90f3ceb0b5c866699a6a9fe8780b1185492c66
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2023-02-05 18:41:37 +0530

    Update __main__.py

--- plugins/mdl/imdb/__main__.py
+++ plugins/mdl/imdb/__main__.py
@@ -8,3 +8,7 @@ from pySmartDL import SmartDL
 THUMB_PATH = str(Path().cwd())
 
 @userge.on_cmd("imdb", about="get movie from imdb")
+async def imdb_(message: Message) -> None:
+  """takes name and search with imdb"""
+  name = message.input_str
+  await message.edit(f"Seachring for : {name}")
