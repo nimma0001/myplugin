be87dfd0f2a4726849c95846200e2958830b5ffa
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2023-02-05 13:18:21 +0530

    Update __main__.py

--- plugins/mdl/pymdl/__main__.py
+++ plugins/mdl/pymdl/__main__.py
@@ -1,1 +1,11 @@
+from userge import userge, Message
 
+@userge.on_cmd("test", about="my first command")
+async def first_command(message: Message) -> None:
+    """ this thing will be used as command doc string """
+
+    test.Dynamic.TIMEOUT = 90
+
+    await call_api()
+
+    await message.edit("first cmd executed")
