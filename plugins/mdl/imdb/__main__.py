dc7c701d2a2a3ea140eed87cfdcdb388d950e47f
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2023-02-05 20:16:49 +0530

    Update __main__.py

--- plugins/mdl/imdb/__main__.py
+++ plugins/mdl/imdb/__main__.py
@@ -12,7 +12,25 @@ async def imdb_(message: Message) -> None:
     """takes name and search with imdb"""
     name = message.input_str
     await message.edit(f"Seachring for : {name}")
-
+    data, image = await search_(name)
+    #os.remove(THUMB_PATH)
+    try:
+        id = SmartDL(image, THUMB_PATH, progress_bar=False)
+        id.start()
+        await message.client.send_photo(
+            chat_id=message.chat.id,
+            photo=id.get_dest(),
+            caption=data,
+            parse_mode=enums.ParseMode.HTML
+        )
+        await message.delete()
+        os.remove(id.get_dest())
+    except:
+        await message.edit(
+        description,
+        disable_web_page_preview=True,
+        parse_mode=enums.ParseMode.HTML
+    )
   
   
   
@@ -21,11 +39,12 @@ async def search_(name):
     imdb = Cinemagoer()
     try:
         if "tt" in name:
-          data = imdb.get_movie(name[2:]).data
+            data = imdb.get_movie(name[2:]).data
         else:
-          data = imdb.search_movie(name)
-        url = data.url
-        image_link = data.thumbnail
+            id = imdb.search_movie(name)[0].movieID
+            data = imdb.get_movie(id).data
+        url = "https://www.imdb.com/title/tt" + data.get("imdbID")
+        image_link = data..get("cover url").split("V1")[0] + "V1_720.jpg"
         mov_name = data.get("title")
         genres = ", ".join(bb.get("genres"))
         mov_rating = data.get("rating")
@@ -36,7 +55,20 @@ async def search_(name):
         year = data.get("original air date")
         info = data.get("plot outline")
         story_line = (info[:95] + '...') if len(info) > 75 else info
-     
+        description = f"""
+<b>Title</b><a href='{image_link}'>🎬</a>: <code>{mov_name}</code>
+<b>>Genres: </b><code>{genres}</code>
+<b>Rating⭐: </b><code>{mov_rating}</code>
+<b>Language: Hindi</b>
+<b>Duration⏳: </b><code>{duration}</code>
+<b>Director📽: </b><code>{director}</code>
+<b>Stars🎭: </b><code>{stars}</code>
+<b>Release Year📅: </b><code>{year}</code>
+<b>Resolution : 480,720,1080</b>
+<b>IMDB :</b> {url}
+<b>Story Line : </b><em>{story_line}</em>"""
+    
+        return description, image_link
     
     except:
         return "Not Found"
