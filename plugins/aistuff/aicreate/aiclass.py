f856aa705541c27bd0205b3e3ce8773380b5f035
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2023-02-18 20:54:06 +0530

    i thin k done

--- plugins/aistuff/aicreate/aiclass.py
+++ plugins/aistuff/aicreate/aiclass.py
@@ -62,7 +62,6 @@ class nimmadev:
                 pixel_drain = subprocess.check_output(['curl', '-g', 'https://pixeldrain.com/api/file/', '--upload-file', path])
                 pixel_link = json.loads(pixel_drain.decode().replace('\n', ''))['id'] 
                 pixel_link = f"https://pixeldrain.com/api/file/{pixel_link}?download"
-                return pixel_link
             else:
                 pixel_link = path
         except FileNotFoundError:
